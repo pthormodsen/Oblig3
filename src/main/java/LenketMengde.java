1ccf2204ebfa42c228339093f1de20ca366b3467
Patrik Thormodsen <patrik.mikal.thormodsen@gmail.com>
2025-03-04 23:57:48 +0100

    Test commit

--- src/main/java/LenketMengde.java
+++ src/main/java/LenketMengde.java
@@ -1,2 +1,66 @@
-public class LenketMengde {
+public class LenketMengde<T> implements MengdeADT<T> {
+    @Override
+    public boolean erTom() {
+        return false;
+    }
+
+    @Override
+    public boolean inneholder(T element) {
+        return false;
+    }
+
+    @Override
+    public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public boolean erLik(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public boolean erDisjunkt(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public MengdeADT<T> union(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public void leggTil(T element) {
+
+    }
+
+    @Override
+    public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+
+    }
+
+    @Override
+    public T fjern(T element) {
+        return null;
+    }
+
+    @Override
+    public T[] tilTabell() {
+        return null;
+    }
+
+    @Override
+    public int antallElementer() {
+        return 0;
+    }
 }
--- src/main/java/TabellMengde.java
+++ src/main/java/TabellMengde.java
@@ -1,2 +1,88 @@
-public class TabellMengde {
+public class TabellMengde<T> implements MengdeADT<T>{
+
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private T[] tab;
+    private int antall;
+
+    public TabellMengde() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @SuppressWarnings("Unchecked")
+    public TabellMengde(int capacity){
+        tab = (T[]) new Object[capacity];
+        antall = 0;
+    }
+
+    @Override
+    public boolean erTom() {
+        return antall == 0;
+    }
+
+    @Override
+    public boolean inneholder(T element) {
+        for(int i = 0; i < antall; i++){
+            if(element.equals(tab[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public boolean erLik(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public boolean erDisjunkt(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public MengdeADT<T> union(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public void leggTil(T element) {
+
+    }
+
+    @Override
+    public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+
+    }
+
+    @Override
+    public T fjern(T element) {
+        return null;
+    }
+
+    @Override
+    public T[] tilTabell() {
+        return null;
+    }
+
+    @Override
+    public int antallElementer() {
+        return 0;
+    }
 }
