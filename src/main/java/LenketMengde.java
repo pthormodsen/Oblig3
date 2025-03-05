f0c2a2f828ff171603dd5e71ca12d7c3d81d61c1
Patrik Thormodsen <patrik.mikal.thormodsen@gmail.com>
2025-03-05 15:00:06 +0100

    LenketMengde Ferdig (-tilTab)

--- src/main/java/LenketMengde.java
+++ src/main/java/LenketMengde.java
@@ -26,61 +26,177 @@ public class LenketMengde<T> implements MengdeADT<T> {
 
     @Override
     public boolean inneholder(T element) {
+        Node temp = forste;
+        while(temp != null){
+            if(temp.data.equals(element)){
+                return true;
+            }
+            temp = temp.neste;
+        }
         return false;
     }
 
     @Override
     public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
-        return false;
+        Node temp = forste;
+
+        while(temp != null){
+            if(!annenMengde.inneholder(temp.data)){
+                return false;
+            }
+            temp = temp.neste;
+        }
+        return true;
     }
 
     @Override
     public boolean erLik(MengdeADT<T> annenMengde) {
-        return false;
+        Node temp = forste;
+
+        if (antall != annenMengde.antallElementer()) {
+            return false;
+        }
+
+        while(temp != null){
+            if(!annenMengde.inneholder(temp.data)){
+                return false;
+            }
+            temp = temp.neste;
+        }
+        return true;
     }
 
     @Override
     public boolean erDisjunkt(MengdeADT<T> annenMengde) {
-        return false;
+        Node temp = forste;
+
+        while(temp != null){
+            if(annenMengde.inneholder(temp.data)){
+                return false;
+            }
+            temp = temp.neste;
+        }
+        return true;
     }
 
     @Override
     public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
-        return null;
+        LenketMengde<T> nyMengde = new LenketMengde<>();
+        Node temp = forste;
+        while(temp != null){
+            if(annenMengde.inneholder(temp.data)) {
+                nyMengde.leggTil(temp.data);
+            }
+            temp = temp.neste;
+        }
+        return nyMengde;
     }
 
     @Override
     public MengdeADT<T> union(MengdeADT<T> annenMengde) {
-        return null;
+        LenketMengde<T> nyMengde = new LenketMengde<>();
+        Node temp = forste;
+
+        while(temp != null){
+            nyMengde.leggTil(temp.data);
+            temp = temp.neste;
+        }
+
+        Node tempAnnenMengde = forste(annenMengde);
+
+        while(tempAnnenMengde != null){
+            if(!nyMengde.inneholder(tempAnnenMengde.data)){
+                nyMengde.leggTil(tempAnnenMengde.data);
+            }
+            tempAnnenMengde = tempAnnenMengde.neste;
+        }
+
+        return nyMengde;
+    }
+
+
+    private Node forste(MengdeADT<T> mengde){
+        if(mengde instanceof LenketMengde){
+            return ((LenketMengde<T>)mengde).forste;
+        }
+        throw new IllegalArgumentException("Mengde er ikke av typen LenketMengde");
     }
 
     @Override
     public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
-        return null;
+        LenketMengde<T> nyMengde = new LenketMengde<>();
+        Node temp = forste;
+
+        while(temp != null){
+            if(!annenMengde.inneholder(temp.data)){
+                nyMengde.leggTil(temp.data);
+            }
+            temp = temp.neste;
+        }
+        return nyMengde;
     }
 
     @Override
     public void leggTil(T element) {
-
+        if (inneholder(element)) {
+            return;
+        }
+        Node nyNode = new Node(element);
+        nyNode.neste = forste;
+        forste = nyNode;
+        antall++;
     }
 
     @Override
     public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+        Node temp = ((LenketMengde<T>) annenMengde).forste;
 
+        while (temp != null) {
+            if (!inneholder(temp.data)) {
+                leggTil(temp.data);
+            }
+            temp = temp.neste;
+        }
     }
 
     @Override
     public T fjern(T element) {
-        return null;
+
+        T elementTilbake = null;
+        Node temp = forste;
+        Node forrige = null;
+
+        while(temp != null){
+            if(element.equals(temp.data)){
+                elementTilbake = temp.data;
+                if(forrige == null){
+                    forste = forste.neste;
+                }else{
+                    forrige.neste = temp.neste;
+                }
+                antall--;
+                break;
+            }
+            forrige = temp;
+            temp = temp.neste;
+        }
+        return elementTilbake;
     }
 
     @Override
     public T[] tilTabell() {
-        return null;
+        T[] tabell = (T[]) new Object[antall];
+        Node temp = forste;
+        int i = 0;
+        while(temp != null){
+            tabell[i++] = temp.data;
+            temp = temp.neste;
+        }
+        return tabell;
     }
 
     @Override
     public int antallElementer() {
-        return 0;
+        return antall;
     }
 }
---
+++ src/test/java/LenketMengdeTest.java
@@ -0,0 +1,181 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LenketMengdeTest {
+
+    private MengdeADT<Integer> mengde1;
+    private MengdeADT<Integer> mengde2;
+    private MengdeADT<Integer> mengde3;
+
+    @BeforeEach
+    void setUp() {
+        mengde1 = new LenketMengde<>();
+        mengde2 = new LenketMengde<>();
+        mengde3 = new LenketMengde<>();
+    }
+
+    @Test
+    void testErTom(){
+        assertTrue(mengde1.erTom());
+    }
+
+    @Test
+    void testLeggTill(){
+        mengde1.leggTil(2);
+        assertFalse(mengde1.erTom());
+    }
+
+    @Test
+    void testInneholder(){
+        mengde1.leggTil(2);
+        assertFalse(mengde1.inneholder(1));
+        assertTrue(mengde1.inneholder(2));
+    }
+
+    @Test
+    void testErDelmengdeAv(){
+        mengde1.leggTil(2);
+        mengde2.leggTil(2);
+        mengde2.leggTil(3);
+        assertTrue(mengde1.erDelmengdeAv(mengde2));
+    }
+
+    @Test
+    void testErLik(){
+        mengde1.leggTil(2);
+        mengde2.leggTil(2);
+        mengde3.leggTil(3);
+        assertTrue(mengde1.erLik(mengde2));
+        assertFalse(mengde1.erLik(mengde3));
+    }
+
+    @Test
+    void testErDisjunkt(){
+        mengde1.leggTil(2);
+        mengde1.leggTil(4);
+
+        mengde2.leggTil(3);
+        mengde2.leggTil(5);
+
+        mengde3.leggTil(2);
+        mengde3.leggTil(5);
+
+        assertTrue(mengde1.erDisjunkt(mengde2));
+        assertFalse(mengde1.erDisjunkt(mengde3));
+    }
+
+    @Test
+    void testSnitt(){
+        mengde1.leggTil(2);
+        mengde1.leggTil(4);
+
+        mengde2.leggTil(2);
+        mengde2.leggTil(5);
+
+        MengdeADT<Integer> snitt = mengde1.snitt(mengde2);
+
+        assertEquals(1, snitt.antallElementer());
+        assertTrue(snitt.inneholder(2));
+    }
+
+    @Test
+    void testUnion(){
+        mengde1.leggTil(2);
+        mengde1.leggTil(4);
+
+        mengde2.leggTil(2);
+        mengde2.leggTil(5);
+
+        MengdeADT<Integer> union = mengde1.union(mengde2);
+
+        assertEquals(3, union.antallElementer());
+        assertTrue(union.inneholder(2));
+        assertTrue(union.inneholder(4));
+        assertTrue(union.inneholder(5));
+    }
+
+    @Test
+    void testMinus() {
+        mengde1.leggTil(2);
+        mengde1.leggTil(4);
+        mengde1.leggTil(6);
+
+        mengde2.leggTil(2);
+        mengde2.leggTil(5);
+
+        MengdeADT<Integer> minus = mengde1.minus(mengde2);
+
+        assertEquals(2, minus.antallElementer());
+        assertTrue(minus.inneholder(4));
+        assertTrue(minus.inneholder(6));
+        assertFalse(minus.inneholder(2));
+    }
+
+    @Test
+    void testLeggTilAlleFra() {
+        mengde1.leggTil(1);
+        mengde1.leggTil(2);
+
+        mengde2.leggTil(2);
+        mengde2.leggTil(3);
+
+        mengde1.leggTilAlleFra(mengde2);
+
+
+        assertEquals(3, mengde1.antallElementer());
+        assertTrue(mengde1.inneholder(1));
+        assertTrue(mengde1.inneholder(2));
+        assertTrue(mengde1.inneholder(3));
+    }
+
+    @Test
+    void testFjern() {
+        mengde1.leggTil(1);
+        mengde1.leggTil(2);
+        mengde1.leggTil(3);
+
+        Integer fjernetElement = mengde1.fjern(2);
+
+        assertEquals(2, fjernetElement);
+        assertFalse(mengde1.inneholder(2));
+
+        assertTrue(mengde1.inneholder(1));
+        assertTrue(mengde1.inneholder(3));
+    }
+
+    @Test
+    void testTilTabell() { //Hater denne
+
+        mengde1.leggTil(1);
+        mengde1.leggTil(2);
+        mengde1.leggTil(3);
+
+        Integer[] tabell = mengde1.tilTabell();
+
+        assertEquals(3, tabell.length);
+
+        List<Integer> list = Arrays.asList(tabell);
+
+        assertTrue(list.contains(1));
+        assertTrue(list.contains(2));
+        assertTrue(list.contains(3));
+    }
+
+
+
+    @Test
+    void testAntallElementer() {
+        mengde1.leggTil(1);
+        mengde1.leggTil(2);
+        mengde1.leggTil(3);
+
+        assertEquals(3, mengde1.antallElementer());
+    }
+
+}
